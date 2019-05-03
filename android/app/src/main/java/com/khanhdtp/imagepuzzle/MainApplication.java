904a851649c8a3fc78fa0010f21c1f1cb365322c
KhanhDTP <khanhdtp@evolableasia.vn>
2019-05-03 15:59:02 +0700

    [link][module] react-native-sound

--- android/app/src/main/java/com/khanhdtp/imagepuzzle/MainApplication.java
+++ android/app/src/main/java/com/khanhdtp/imagepuzzle/MainApplication.java
@@ -3,6 +3,7 @@ package com.khanhdtp.imagepuzzle;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNSoundPackage(),
         new RNCameraPackage(),
         new RNGestureHandlerPackage()
       );
